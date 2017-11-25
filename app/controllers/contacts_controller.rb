748b72eec4c9787b32633d1b379bb061d39955d5
Aaron Goodrum <ag505181@gmail.com>
2017-11-25 00:51:21 +0000

    Cleaned up contact form routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,16 +2,16 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-def create
-  @contact = Contact.new(contact_params)
-  if @contact.save
-     flash[:success] = "Message sent."
-     redirect_to new_contact_path
-  else
-     flash[:danger] = @contact.errors.full_messages.join(", ")
-     redirect_to new_contact_path
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+       flash[:success] = "Message sent."
+       redirect_to new_contact_path
+    else
+       flash[:danger] = @contact.errors.full_messages.join(", ")
+       redirect_to new_contact_path
+    end
   end
-end
   private
     def contact_params
        params.require(:contact).permit(:name, :email, :comments)
