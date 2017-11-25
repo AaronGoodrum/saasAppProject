148e6fb9e265dba81c615d756a623e1369801e47
Aaron Goodrum <ag505181@gmail.com>
2017-11-25 02:30:07 +0000

    added code comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,23 @@
 class ContactsController < ApplicationController
+  
+  #GET request to /contact-us
+  #Show new contact form
   def new
     @contact = Contact.new
   end
+  
+  #Post request /contacts
   def create
+    #Mass assingment of form fields into Contact object
     @contact = Contact.new(contact_params)
+    
+    #Save the Contact Object to the database
     if @contact.save
         name = params[:contact][:name]
         email = params[:contact][:email]
         body = params[:contact][:comments]
+      
+      #flash success from Contact form and send email
         ContactMailer.contact_email(name, email, body).deliver
        flash[:success] = "Message sent."
        redirect_to new_contact_path
