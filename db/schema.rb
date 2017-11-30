045dc5c4df8a42fcf7cee3bd9a0ca8b096e252be
Aaron Goodrum <ag505181@gmail.com>
2017-11-30 03:41:13 +0000

    added databases for plans and group plan

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130031402) do
+ActiveRecord::Schema.define(version: 20171130032647) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
